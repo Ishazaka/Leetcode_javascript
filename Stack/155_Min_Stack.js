974cad000b1d9da834b246ac588cee3b310607ab
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-11-12 19:25:27 -0500

    Medium

--- Stack/155_Min_Stack.js
+++ Stack/155_Min_Stack.js
@@ -55,27 +55,101 @@ class MinStack {
 // Space complexity : O(n)
 
 
-class MinStack:
-    def __init__(self):
-        self.stack = []
-        self.minStack = []
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
 
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-        val = min(val, self.minStack[-1] if self.minStack else val)
-        self.minStack.append(val)
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        val = Math.min(
+            val,
+            this.minStack.length === 0
+                ? val
+                : this.minStack[this.minStack.length - 1],
+        );
+        this.minStack.push(val);
+    }
 
-    def pop(self) -> None:
-        self.stack.pop()
-        self.minStack.pop()
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
 
-    def top(self) -> int:
-        return self.stack[-1]
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
 
-    def getMin(self) -> int:
-        return self.minStack[-1]
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
 
 
 
 
+//One stack
+// Time complexity: O(1) for all operations
+// Space complexity : O(n)
 
+
+class MinStack {
+    constructor() {
+        this.min = Infinity;
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        if (this.stack.length === 0) {
+            this.stack.push(0);
+            this.min = val;
+        } else {
+            this.stack.push(val - this.min);
+            if (val < this.min) this.min = val;
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        if (this.stack.length === 0) return;
+
+        const pop = this.stack.pop();
+
+        if (pop < 0) this.min -= pop;
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        const top = this.stack[this.stack.length - 1];
+        return top > 0 ? top + this.min : this.min;
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.min;
+    }
+}

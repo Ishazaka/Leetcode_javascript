56044549d0ee560b8fc802030c2b2dfdcdf92e53
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-12-03 21:08:32 -0500

    Easy

--- Array/121_Best_Time_to_Buy_and_Sell_Stock.js
+++ Array/121_Best_Time_to_Buy_and_Sell_Stock.js
@@ -16,3 +16,29 @@ class Solution {
         return res;
     }
 }
+
+
+//two pointers
+//2nd solution
+
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let l = 0, r = 1;
+        let maxP = 0;
+
+        while (r < prices.length) {
+            if (prices[l] < prices[r]) {
+                let profit = prices[r] - prices[l];
+                maxP = Math.max(maxP, profit);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+        return maxP;
+    }
+}

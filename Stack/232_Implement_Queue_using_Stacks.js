bc1ce306e5ba748184af97d385e754b968210f95
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-01-20 20:19:50 -0500

    Easy

--- Stack/232_Implement_Queue_using_Stacks.js
+++ Stack/232_Implement_Queue_using_Stacks.js
@@ -51,3 +51,55 @@ class MyQueue {
         return this.stack1.length === 0;
     }
 }
+
+
+
+// 2nd way
+
+class MyQueue {
+    constructor() {
+        this.s1 = [];
+        this.s2 = [];
+    }
+
+    /** 
+     * @param {number} x
+     * @return {void}
+     */
+    push(x) {
+        this.s1.push(x);
+    }
+
+    /**
+     * @return {number}
+     */
+    pop() {
+        if (this.s2.length === 0) {
+            while (this.s1.length > 0) {
+                this.s2.push(this.s1.pop());
+            }
+        }
+        return this.s2.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    peek() {
+        if (this.s2.length === 0) {
+            while (this.s1.length > 0) {
+                this.s2.push(this.s1.pop());
+            }
+        }
+        return this.s2[this.s2.length - 1];
+    }
+
+    /**
+     * @return {boolean}
+     */
+    empty() {
+        return this.s1.length === 0 && this.s2.length === 0;
+    }
+}
+
+

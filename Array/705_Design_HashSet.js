cab4d8f42fbf2bce0a38b7cbca80f67543a3b0ae
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-01-26 22:45:50 -0500

    Easy

--- Array/705_Design_HashSet.js
+++ Array/705_Design_HashSet.js
@@ -70,3 +70,74 @@ class MyHashSet {
 
 
 
+
+//3rd way
+
+class ListNode {
+    /**
+     * @param {number} key
+     */
+    constructor(key) {
+        this.key = key;
+        this.next = null;
+    }
+}
+
+class MyHashSet {
+    constructor() {
+        this.set = Array.from({ length: 10000 }, () => new ListNode(0));
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    hash(key) {
+        return key % this.set.length;
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        let cur = this.set[this.hash(key)];
+        while (cur.next) {
+            if (cur.next.key === key) {
+                return;
+            }
+            cur = cur.next;
+        }
+        cur.next = new ListNode(key);
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        let cur = this.set[this.hash(key)];
+        while (cur.next) {
+            if (cur.next.key === key) {
+                cur.next = cur.next.next;
+                return;
+            }
+            cur = cur.next;
+        }
+    }
+
+    /**
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key) {
+        let cur = this.set[this.hash(key)];
+        while (cur.next) {
+            if (cur.next.key === key) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+}

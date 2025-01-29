c2dd8325314c5ac4d1545440e5bb43a97320bce7
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-01-28 20:58:08 -0500

    Easy

--- Array/705_Design_HashSet.js
+++ Array/705_Design_HashSet.js
@@ -279,3 +279,49 @@ class MyHashSet {
     }
 }
 
+
+
+// 5th way
+
+
+class MyHashSet {
+    constructor() {
+        // key is in the range [1, 1000000]
+        // 31251 * 32 = 1000032
+        this.set = new Array(31251).fill(0);
+    }
+
+    /** 
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        this.set[Math.floor(key / 32)] |= this.getMask(key);
+    }
+
+    /** 
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        if (this.contains(key)) {
+            this.set[Math.floor(key / 32)] ^= this.getMask(key);
+        }
+    }
+
+    /** 
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key) {
+        return (this.set[Math.floor(key / 32)] & this.getMask(key)) !== 0;
+    }
+
+    /** 
+     * @param {number} key
+     * @return {number}
+     */
+    getMask(key) {
+        return 1 << (key % 32);
+    }
+}

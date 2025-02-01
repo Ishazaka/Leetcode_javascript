037e81849268bdca1499dc1335252e72c47e74ad
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-01-31 19:36:00 -0500

    Medium

--- Linked_List/622_Design_Circular_Queue.js
+++ Linked_List/622_Design_Circular_Queue.js
@@ -60,3 +60,76 @@ class MyCircularQueue {
         return this.queue.length === this.capacity;
     }
 }
+
+
+
+// Array
+
+class MyCircularQueue {
+    /**
+     * @param {number} k
+     */
+    constructor(k) {
+        this.queue = new Array(k);
+        this.size = 0;
+        this.front = 0;
+        this.rear = -1;
+        this.capacity = k;
+    }
+
+    /**
+     * @param {number} value
+     * @return {boolean}
+     */
+    enQueue(value) {
+        if (this.isFull()) {
+            return false;
+        }
+        this.rear = (this.rear + 1) % this.capacity;
+        this.queue[this.rear] = value;
+        this.size++;
+        return true;
+    }
+
+    /**
+     * @return {boolean}
+     */
+    deQueue() {
+        if (this.isEmpty()) {
+            return false;
+        }
+        this.front = (this.front + 1) % this.capacity;
+        this.size--;
+        return true;
+    }
+
+    /**
+     * @return {number}
+     */
+    Front() {
+        return this.isEmpty() ? -1 : this.queue[this.front];
+    }
+
+    /**
+     * @return {number}
+     */
+    Rear() {
+        return this.isEmpty() ? -1 : this.queue[this.rear];
+    }
+
+    /**
+     * @return {boolean}
+     */
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    /**
+     * @return {boolean}
+     */
+    isFull() {
+        return this.size === this.capacity;
+    }
+}
+
+

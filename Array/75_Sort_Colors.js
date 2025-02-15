9c22c45e56f29c4de1cca2e03ae63d7cbeef9289
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-02-14 22:45:05 -0500

    Medium

--- Array/75_Sort_Colors.js
+++ Array/75_Sort_Colors.js
@@ -1,4 +1,5 @@
 
+//1st way
 class Solution {
     /**
      * @param {number[]} nums
@@ -6,5 +7,28 @@ class Solution {
      */
     sortColors(nums) {
         nums.sort((a, b) => a - b);
+    }
+}
+
+
+//2nd way
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        const count = new Int32Array(3);
+        for (let num of nums) {
+            count[num]++;
+        }
+
+        let index = 0;
+        for (let i = 0; i < 3; i++) {
+            while (count[i]-- > 0) {
+                nums[index++] = i;
+            }
+        }
     }
 }

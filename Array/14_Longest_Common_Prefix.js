8a3d462c075d1264998a0cd910f500f0ec7787fe
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-02-21 17:47:13 -0500

    Easy

--- Array/14_Longest_Common_Prefix.js
+++ Array/14_Longest_Common_Prefix.js
@@ -20,3 +20,25 @@ class Solution {
         return prefix;
     }
 }
+
+
+
+// Vertical Scanning
+
+
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        for (let i = 0; i < strs[0].length; i++) {
+            for (let s of strs) {
+                if (i === s.length || s[i] !== strs[0][i]) {
+                    return s.slice(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}

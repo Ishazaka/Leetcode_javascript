5dc3355b4ab94e9331f17bd4c85746ecfa951fc6
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-02-24 22:07:31 -0500

    Easy

--- Array/14_Longest_Common_Prefix.js
+++ Array/14_Longest_Common_Prefix.js
@@ -67,3 +67,81 @@ class Solution {
         return strs[0];
     }
 }
+
+
+
+// 4th way
+
+
+class TrieNode {
+    constructor() {
+        this.children = {};
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {
+        let node = this.root;
+        for (let char of word) {
+            if (!node.children[char]) {
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+        }
+    }
+
+    /**
+     * @param {string} word
+     * @param {number} prefixLen
+     * @return {number}
+     */
+    lcp(word, prefixLen) {
+        let node = this.root;
+        let i = 0;
+        while (i < Math.min(word.length, prefixLen)) {
+            if (!node.children[word[i]]) {
+                return i;
+            }
+            node = node.children[word[i]];
+            i++;
+        }
+        return Math.min(word.length, prefixLen);
+    }
+}
+
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        if (strs.length === 1) {
+            return strs[0];
+        }
+        
+        let mini = 0;
+        for (let i = 1; i < strs.length; i++) {
+            if (strs[mini].length > strs[i].length) {
+                mini = i;
+            }
+        }
+
+        const trie = new Trie();
+        trie.insert(strs[mini]);
+        let prefixLen = strs[mini].length;
+
+        for (let i = 0; i < strs.length; i++) {
+            prefixLen = trie.lcp(strs[i], prefixLen);
+        }
+
+        return strs[0].substring(0, prefixLen);
+    }
+}

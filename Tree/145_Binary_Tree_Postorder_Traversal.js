4afcd8a30f474604d61f5e13cbb6140900ce3638
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-03-09 21:30:20 -0400

    Easy

--- Tree/145_Binary_Tree_Postorder_Traversal.js
+++ Tree/145_Binary_Tree_Postorder_Traversal.js
@@ -1,3 +1,7 @@
+
+// 1st way
+// Depth First Search
+
 /**
  * Definition for a binary tree node.
  * class TreeNode {
@@ -24,6 +28,53 @@ class Solution {
         };
 
         postorder(root);
+        return res;
+    }
+}
+
+
+
+// 2nd way
+// Iterative Depth First Search - I
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    postorderTraversal(root) {
+        const stack = [root];
+        const visit = [false];
+        const res = [];
+
+        while (stack.length) {
+            const cur = stack.pop();
+            const v = visit.pop();
+
+            if (cur) {
+                if (v) {
+                    res.push(cur.val);
+                } else {
+                    stack.push(cur);
+                    visit.push(true);
+                    stack.push(cur.right);
+                    visit.push(false);
+                    stack.push(cur.left);
+                    visit.push(false);
+                }
+            }
+        }
+
         return res;
     }
 }

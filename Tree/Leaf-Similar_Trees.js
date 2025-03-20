0486b27524749c99b854d2d03868b97d3bfa61d0
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2025-03-19 23:05:46 -0400

    Easy

--- Tree/Leaf-Similar_Trees.js
+++ Tree/Leaf-Similar_Trees.js
@@ -82,3 +82,44 @@ class Solution {
         return dfs1(root2, leaf1) && leaf1.length === 0;
     }
 }
+
+
+
+// 3rd way
+
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root1
+     * @param {TreeNode} root2
+     * @return {boolean}
+     */
+    leafSimilar(root1, root2) {
+        const getPathLeaf = (stack) => {
+            while (stack.length) {
+                const node = stack.pop();
+                if (node.right) stack.push(node.right);
+                if (node.left) stack.push(node.left);
+                if (!node.left && !node.right) return node.val;
+            }
+        };
+
+        const stack1 = [root1], stack2 = [root2];
+        while (stack1.length && stack2.length) {
+            if (getPathLeaf(stack1) !== getPathLeaf(stack2)) {
+                return false;
+            }
+        }
+        return stack1.length === 0 && stack2.length === 0;
+    }
+}
